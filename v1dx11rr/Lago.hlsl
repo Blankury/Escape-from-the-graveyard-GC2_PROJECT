9fbf552b9800953c8ee3051be25c1e1813baf4fb
Blankury <elizabeth.delgadillotrj@uanl.edu.mx>
2022-03-21 15:58:37 -0600

    Agua

---
+++ v1dx11rr/Lago.hlsl
@@ -0,0 +1,177 @@
+Texture2D colorMap : register(t0);
+Texture2D normalMap : register(t1);
+Texture2D dispMap : register(t2);
+
+SamplerState colorSampler : register(s0);
+
+cbuffer cbChangerEveryFrame : register(b0)
+{
+    matrix worldMatrix;
+};
+
+cbuffer cbNeverChanges : register(b1)
+{
+    matrix viewMatrix;
+};
+
+cbuffer cbChangeOnResize : register(b2)
+{
+    matrix projMatrix;
+};
+
+
+
+cbuffer cbChangerotation : register(b3)
+{
+    matrix rotationMat;
+};
+
+
+
+cbuffer cbChangemov : register(b4)
+{
+    float3 movimiento;
+};
+
+
+
+
+struct VS_Input
+{
+    float4 pos : POSITION;
+    float2 tex0 : TEXCOORD0;
+};
+
+struct PS_Input
+{
+    float4 pos : SV_POSITION;
+    float2 tex0 : TEXCOORD0;
+    
+    float4 espacio : TEXCOORD1;
+};
+
+PS_Input VS_Main(VS_Input vertex)
+{
+    
+    
+    PS_Input vsOut = (PS_Input) 0;
+    vsOut.pos = mul(vertex.pos, worldMatrix);
+    vsOut.pos = mul(vsOut.pos, viewMatrix);
+    vsOut.pos = mul(vsOut.pos, projMatrix);
+
+    float tile = 6.0;
+    vsOut.tex0 = vertex.tex0;
+
+    //vsOut.pos = vertex.pos;
+    vsOut.espacio = vsOut.pos;
+
+    return vsOut;
+    
+}
+
+float4 PS_Main(PS_Input pix) : SV_TARGET
+{
+    float3 ambient = float3(0.1f, 0.1f, 0.1f);
+    const float waveStrenght = 0.5;
+    
+    //coordenadas normalizadas que serán las coords de refraccion
+    float2 ndc = (pix.espacio.xy / pix.espacio.w) / 2.0 + 0.5;
+    float2 refractTexCoords = float2(ndc.x, -ndc.y);
+    //coordenadas para la distorsion para que parezca que tiene ondas
+    float2 distortion, distortion2;
+	////Usamos la variable movimiento que moverá la distorsión en x y y
+    distortion = (dispMap.Sample(colorSampler, (pix.tex0.x + movimiento.x, pix.tex0.y)).rg * 2.0 - 1.0) * 0.2;
+    distortion2 = (dispMap.Sample(colorSampler, (-pix.tex0.x + movimiento.x, pix.tex0.y + movimiento.x)).rg * 2.0 - 1.0) * 0.2;
+	//Suma las distorciones
+    float2 Total = distortion + distortion2;
+    //A la refracción le sumamos el total de la distorsión para hacerla una sola coordenada
+    refractTexCoords += Total;
+    refractTexCoords.x = clamp(refractTexCoords.x, 0.001, 0.999);
+    refractTexCoords.y = clamp(refractTexCoords.y, -0.999, -0.001);
+    
+    
+    
+    
+    
+    float4 text = colorMap.Sample(colorSampler, refractTexCoords);
+
+    //float4 text = colorMap.Sample(colorSampler, pix.tex0);
+	//float intensity = 0.4;
+    
+	//if(text.r >= intensity && text.g >= intensity && text.b >= intensity)
+    
+   
+	//aqui se calcula la iluminacion difusa
+    float4 textnorm = normalMap.Sample(colorSampler, pix.tex0);
+    float4 LuzAmbiental = float4(1.0, 1.0, 1.0, 1);
+    float FA = 0.3;
+	///////////////////////////////////////////////
+	// aqui se desarrolla el elemento ambiental
+	///////////////////////////////////////////////
+    float4 AportAmb = LuzAmbiental * FA;
+	//////////////////////////////////////////
+	//aqui se desarrolla el elemento difuso //
+	//////////////////////////////////////////
+    float3 DirLuz = float3(30, 10, 30);
+    float4 LuzDifusa = float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float FAD = 1;
+	
+    float3 bump = normalize(2.0 * textnorm - 1.0);   
+    float FALL = dot(normalize(bump), normalize(DirLuz));
+    float4 AportLuzDif = saturate(LuzDifusa * FALL * FAD);
+        
+    //Salida de color
+    text = text * (AportAmb + AportLuzDif);
+    text.a = 1;
+    return text;
+    
+}
+        
+ //   float4 LuzAmbiental = float4(0.1f, 0.1f, 0.1f, 1.0f);
+ //   float FAD = 0.8f;
+ //   float3 DirLuz = float3(30, 10, 30);
+    
+ //   const float waveStrenght = 0.5;
+	////coordenadas normalizadas que serán las coords de refraccion
+ //   float2 ndc = (pix.espacio.xy / pix.espacio.w) / 2.0 + 0.5;
+ //   float2 refractTexCoords = float2(ndc.x, -ndc.y);
+	
+	////coordenadas para la distorsion para que parezca que tiene ondas
+ //   float2 distortion, distortion2;
+	//////Usamos la variable movimiento que moverá la distorsión en x y y
+ //   distortion = (dispMap.Sample(colorSampler, (pix.tex0.x + movimiento.x, pix.tex0.y)).rg * 2.0 - 1.0) * 0.2;
+ //   distortion2 = (dispMap.Sample(colorSampler, (-pix.tex0.x + movimiento.x, pix.tex0.y + movimiento.x)).rg * 2.0 - 1.0) * 0.2;
+	////Suma las distorciones
+ //   float2 Total = distortion + distortion2;
+    
+ //   //A la refracción le sumamos el total de la distorsión para hacerla una sola coordenada
+ //   refractTexCoords += Total;
+ //   refractTexCoords.x = clamp(refractTexCoords.x, 0.001, 0.999);
+ //   refractTexCoords.y = clamp(refractTexCoords.y, -0.999, -0.001);
+
+	//////Color de textura
+ //   float4 textureColor = colorMap.Sample(colorSampler, refractTexCoords);
+
+ //   //Aporte ambiental
+ //   float4 AportAmb = LuzAmbiental * 0.3; //*FA
+    
+ //   //Aporte difuso
+ //   float4 txtnrm = normalMap.Sample(colorSampler, pix.tex0);
+    
+ //   float3 bump = normalize(2.0 * txtnrm - 1);
+ //   float3 bumpnormal = normalize(mul(rotationMat, float4(bump, 1.0)));
+ //   float FALL = dot(bumpnormal, normalize(DirLuz));
+ //   float diff = saturate(dot(normalize(DirLuz), bumpnormal));
+    
+ //   float4 luzdifusafinal = float4(0.0f, 0.0f, 0.0f, 1.0f);
+ //   float4 AportDif = saturate(luzdifusafinal * FALL * FAD);
+ //   //float4 luzdifusafinal = ColorLuzDifusa * (1.0f) * diff;
+
+ //   textureColor = normalize(textureColor * (AportAmb + AportDif));
+ //   //textureColor = normalize(textureColor);
+	////Salida de color
+ //   textureColor.a = 0.5;
+   
+ //   return textureColor;
+
+
